440f354430e132b7b4220e0355fed4028959a808
mikejpoole <mike@yachting.org>
2021-03-10 03:57:41 +0000

    Added an example row in orderline

--- migrations/sqls/20210309163026-orderline-up.sql
+++ migrations/sqls/20210309163026-orderline-up.sql
@@ -4,3 +4,7 @@ CREATE TABLE "orderline" (
     product_id BIGINT REFERENCES product(id),
     quantity INTEGER
 );
+
+INSERT INTO "orderline" (order_id, product_id, quantity) values (1, 1, 1);
+-- INSERT INTO "orderline" (order_id, product_id, quantity) values (1, 2, 5);
+-- INSERT INTO "orderline" (order_id, product_id, quantity) values (1, 3, 10);
