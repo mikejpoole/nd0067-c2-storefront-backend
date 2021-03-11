f47f9743c7f28ee32b9f5838de48c6812d3aa7ed
mikejpoole <mike@yachting.org>
2021-03-11 17:49:31 +0000

    Added cors with default options to router

--- src/handler/routes.ts
+++ src/handler/routes.ts
@@ -1,9 +1,21 @@
 import express from 'express';
+import cors from "cors";
+
 import orderRouter from './order';
 import productRouter from './product';
 import userRouter from './user';
 
 const router = express.Router();
+
+// default options from https://expressjs.com/en/resources/middleware/cors.html
+const corsOptions = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+};
+
+router.use(cors(corsOptions));
 
 router.get( "/", ( req, res ) => {
     res.status(200).send(`<html style="background: #f5f5f5; text-align: center;">
